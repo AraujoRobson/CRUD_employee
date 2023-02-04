306effdd6b6d913502f8cd3ff65c4359e774bae0
AraujoRobson <roobson.araujo02@gmail.com>
2023-02-04 10:30:30 -0300

    config cors

--- backend/crud_employee/src/main/java/br/edu/unoesc/crud_employee/CrudEmployeeApplication.java
+++ backend/crud_employee/src/main/java/br/edu/unoesc/crud_employee/CrudEmployeeApplication.java
@@ -2,6 +2,9 @@ package br.edu.unoesc.crud_employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CrudEmployeeApplication {
@@ -9,5 +12,16 @@ public class CrudEmployeeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudEmployeeApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST", "PUT",
+						"DELETE");
+			}
+		};
+	}
 }

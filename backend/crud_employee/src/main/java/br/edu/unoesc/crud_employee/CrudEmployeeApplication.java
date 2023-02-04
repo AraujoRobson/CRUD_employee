4ae33463738dde8fff39f6f6dd67358768feec46
AraujoRobson <roobson.araujo02@gmail.com>
2023-02-04 09:34:22 -0300

    first commit

---
+++ backend/crud_employee/src/main/java/br/edu/unoesc/crud_employee/CrudEmployeeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.crud_employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmployeeApplication.class, args);
+	}
+
+}
